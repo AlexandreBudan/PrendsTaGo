b37adb38f942ecf8230833a83b358d8daa67e629
AlexandreBudan <luckynightm4re@gmail.com>
2023-11-09 19:38:20 +0100

    init project

--- db.sql
+++ db.sql
@@ -5,6 +5,22 @@
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `Adress`
+--
+
+DROP TABLE IF EXISTS `Adress`;
+CREATE TABLE IF NOT EXISTS `Adress` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `address` VARCHAR(255) NOT NULL,
+    `city` VARCHAR(255) NOT NULL,
+    `code` VARCHAR(20) NOT NULL,
+    `country` VARCHAR(255) NOT NULL,
+    `status` TINYINT NOT NULL
+);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `User`
 --
 
@@ -12,35 +28,33 @@ DROP TABLE IF EXISTS `User`;
 CREATE TABLE IF NOT EXISTS `User` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `email` VARCHAR(255) NOT NULL,
-    `roles` JSON,
     `password` VARCHAR(255) NOT NULL,
-    `firstName` VARCHAR(255),
-    `lastName` VARCHAR(255),
-    `phone` VARCHAR(20),
-    `age` INT,
-    `gender` VARCHAR(10),
-    `address_id` INT,
-    `creationDate` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `newsLetter` TINYINT,
-    `verified` TINYINT,
-    `status` TINYINT,
-    FOREIGN KEY (`address_id`) REFERENCES `Address`(`address_id`)
+    `firstName` VARCHAR(255) NOT NULL,
+    `lastName` VARCHAR(255) NOT NULL,
+    `phone` VARCHAR(20) NOT NULL,
+    `age` INT NOT NULL,
+    `gender` VARCHAR(10) NOT NULL,
+    `addressId` INT,
+    `creationDate` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `newsLetter` TINYINT NOT NULL,
+    `verified` TINYINT NOT NULL,
+    `status` TINYINT NOT NULL,
+    FOREIGN KEY (`addressId`) REFERENCES `Address`(`id`)
 );
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Adress`
+-- Structure de la table `Role`
 --
 
-DROP TABLE IF EXISTS `Adress`;
-CREATE TABLE IF NOT EXISTS `Adress` (
+DROP TABLE IF EXISTS `Role`;
+CREATE TABLE IF NOT EXISTS `Role` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `address` VARCHAR(255),
-    `city` VARCHAR(255),
-    `code` VARCHAR(20),
-    `country` VARCHAR(255),
-    `status` TINYINT
+    `userId` INT NOT NULL,
+    `role` VARCHAR(20) NOT NULL,
+    `status` TINYINT NOT NULL,
+    FOREIGN KEY (`userId`) REFERENCES `User`(`id`)
 );
 
 -- --------------------------------------------------------
@@ -52,58 +66,32 @@ CREATE TABLE IF NOT EXISTS `Adress` (
 DROP TABLE IF EXISTS `Car`;
 CREATE TABLE IF NOT EXISTS `Car` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(255),
-    `brand` VARCHAR(255),
-    `passengers` INT,
-    `price` INT,
-    `manual` TINYINT,
-    `type` VARCHAR(255),
+    `name` VARCHAR(255) NOT NULL,
+    `brand` VARCHAR(255) NOT NULL,
+    `passengers` INT NOT NULL,
+    `price` INT NOT NULL,
+    `manual` TINYINT NOT NULL,
+    `type` VARCHAR(255) NOT NULL,
     `minAge` INT,
-    `nbDoor` INT,
+    `nbDoor` INT NOT NULL,
     `location` VARCHAR(255),
-    `unavailables` JSON,
-    `status` TINYINT
+    `status` TINYINT NOT NULL
 );
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Pilote`
+-- Structure de la table `UnvailableDate`
 --
 
-DROP TABLE IF EXISTS `Pilote`;
-CREATE TABLE IF NOT EXISTS `Pilote` (
+DROP TABLE IF EXISTS `UnvailableDate`;
+CREATE TABLE IF NOT EXISTS `UnvailableDate` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `firstName` VARCHAR(255),
-    `lastName` VARCHAR(255),
-    `email` VARCHAR(255),
-    `phone` VARCHAR(20),
-    `status` TINYINT
-);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `Reservation`
---
-
-DROP TABLE IF EXISTS `Reservation`;
-CREATE TABLE IF NOT EXISTS `Reservation` (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `carId` INT,
-    `userId` INT,
-    `protection` TINYINT,
-    `price` INT,
-    `beginning` DATE,
-    `ending` DATE,
-    `nbDoor` INT,
-    `finish` TINYINT,
-    `beginningState` VARCHAR(255),
-    `endingState` VARCHAR(255),
-    `addFees` FLOAT,
-    `status` TINYINT,
-    FOREIGN KEY (`carId`) REFERENCES `Car`(`id`),
-    FOREIGN KEY (`userId`) REFERENCES `User`(`id`)
+    `carId` INT NOT NULL,
+    `beginning` DATE NOT NULL,
+    `ending` DATE NOT NULL,
+    `status` TINYINT NOT NULL,
+    FOREIGN KEY (`carId`) REFERENCES `Car`(`id`)
 );
 
 -- --------------------------------------------------------
@@ -115,12 +103,54 @@ CREATE TABLE IF NOT EXISTS `Reservation` (
 DROP TABLE IF EXISTS `Commentary`;
 CREATE TABLE IF NOT EXISTS `Commentary` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `carId` INT,
-    `userId` INT,
-    `commentary` TEXT,
-    `rank` INT,
-    `creationDate` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `status` TINYINT,
+    `carId` INT NOT NULL,
+    `userId` INT NOT NULL,
+    `commentary` TEXT NOT NULL,
+    `rank` INT NOT NULL,
+    `creationDate` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `status` TINYINT NOT NULL,
     FOREIGN KEY (`carId`) REFERENCES `Car`(`id`),
     FOREIGN KEY (`userId`) REFERENCES `User`(`id`)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Reservation`
+--
+
+DROP TABLE IF EXISTS `Reservation`;
+CREATE TABLE IF NOT EXISTS `Reservation` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `carId` INT NOT NULL,
+    `userId` INT NOT NULL,
+    `protection` TINYINT NOT NULL,
+    `price` INT NOT NULL,
+    `beginning` DATE NOT NULL,
+    `ending` DATE NOT NULL,
+    `finish` TINYINT NOT NULL,
+    `beginningState` VARCHAR(255) NOT NULL,
+    `endingState` VARCHAR(255) NOT NULL,
+    `addFees` FLOAT,
+    `status` TINYINT NOT NULL,
+    FOREIGN KEY (`carId`) REFERENCES `Car`(`id`),
+    FOREIGN KEY (`userId`) REFERENCES `User`(`id`)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Pilote`
+--
+
+DROP TABLE IF EXISTS `Pilote`;
+CREATE TABLE IF NOT EXISTS `Pilote` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `reservationId` INT NOT NULL,
+    `firstName` VARCHAR(255) NOT NULL,
+    `lastName` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `phone` VARCHAR(20) NOT NULL,
+    `status` TINYINT NOT NULL,
+    FOREIGN KEY (`reservationId`) REFERENCES `Reservation`(`id`)
 );
